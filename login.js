754d04f0294d1be20f03f65c6591657391e750a1
vousya <volodyaoyan@gmail.com>
2025-04-15 18:11:14 +0300

    login

---
+++ login.js
@@ -0,0 +1,28 @@
+document.getElementById("loginForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const response = await fetch("http://localhost:8000/token", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: new URLSearchParams({ 
+          grant_type: "password",
+          username: email,
+          password: password
+      })
+      });
+
+
+    const data = await response.json();
+
+    if (response.ok) {
+      localStorage.setItem("access_token", data.access_token);
+      window.location.href = "/student.html"; // or wherever you want to go
+    } else {
+      document.getElementById("error").innerText = data.detail || "Login failed";
+    }
+  });
